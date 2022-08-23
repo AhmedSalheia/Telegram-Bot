8bf94d696dc8e4b924234290972c55ee26ce6dd0
Ahmed Salheia <ahmedsalheia.as@gmail.com>
2022-08-23 20:02:58 +0300

    added params to the routes

--- index.php
+++ index.php
@@ -5,13 +5,14 @@ use \TelegramBot\Bot;
 use \TelegramBot\Components\Router;
 use \TelegramBot\Components\Response;
 
-$bot = new Bot('Your Telegram Token');
+$bot = new Bot('5362393291:AAFUDrCFU_yvq6QJN20eujyOZgAeHIqdbiE');
 if (@$_SERVER['CONTENT_TYPE']===null) {
     $bot::setupWebsocket();
     exit();
 }
 
-Router::input('/start',function () use ($bot){
+Router::input('/start::id::',function ($id) use ($bot){
+    return Response::sendMessage()->text($id??'id is null');
     // inside the input use {{$bot->update()->message()}} to get your message data...
     $text = "Hi ".$bot->update()->message()->from()->username." Your ID is: ".$bot->update()->message()->from()->id;
     return Response::sendMessage()->text($text);
--- src/Components/Router.php
+++ src/Components/Router.php
@@ -31,19 +31,30 @@ class Router
 
         if ($route === []) $route = Bot::update()->getRoute();
         $type = $route['type'].'s';
-        $key = strtolower(str_replace(self::$prefixes[$type],'',$route['route']));
+        $params = explode(' ',strtolower(str_replace(self::$prefixes[$type],'',$route['route'])));
+        $key = array_shift($params);
         if (!array_key_exists($key,self::$$type))
             if (($key = self::$step) !== '' && !empty($key) && array_key_exists($key, self::$steps)) $type = 'steps';
             else $key = 'fallback.404';
 
-        return self::return($key, $type);
+        return self::return($key, $type, $params);
     }
-    private static function return($key, $type)
+    private static function return($key, $type, $params=[])
     {
         $ref = (self::$$type)[$key];
+        $paramValues = [];
+        if ($ref['params'])
+            foreach ($ref['params'] as $param=>$optional)
+            {
+                if ($v = array_shift($params))
+                    $paramValues[$param] = $v;
+                elseif (!$optional) return self::return('fallback.404', ($type === 'steps') ? 'inputs' : $type);
+                else $paramValues[$param] = null;
+            }
+
         if (!$ref['admin'] || (Bot::is_admin()))
         {
-            $return = (new self())->getReference($ref['value']);
+            $return = (new self())->getReference($ref['value'], $paramValues);
             if ($return instanceof Send)
                 return $return->execute(false);
             else
@@ -51,47 +62,60 @@ class Router
         }
         return self::return('fallback.403', ($type === 'steps') ? 'inputs' : $type);
     }
-    private function getReference(\Closure|array|string $value)
+    private function getReference(\Closure|array|string $value, $params)
     {
-        if ($value instanceof \Closure) return $value();
-        elseif(is_array($value)) return (new $value[0])->{$value[1]}();
+        if ($value instanceof \Closure) return $value(...$params);
+        elseif(is_array($value)) return (new $value[0])->{$value[1]}(...$params);
         else {
             $value = explode('@',$value);
-            return (new (self::$namespace . $value[0]))->{$value[1]}();
+            return (new (self::$namespace . $value[0]))->{$value[1]}(...$params);
         }
     }
     public static function getStepFrom($stepFrom)
     {
         self::$step = $stepFrom;
     }
-    private static function getKey($key, $prefixes, &$admin)
+    private static function getKey($key, $prefixes, &$admin, &$params)
     {
         $key = strtolower(str_replace($prefixes,'',$key));
+        preg_match_all('/::[a-zA-Z\d_\-]+\??::/',$key, $matches);
+
+        foreach ($matches[0] as $el)
+        {
+            $e = trim($el, ':');
+            $params[trim($e,'?')] = str_ends_with($e, '?');
+        }
+        $key = preg_replace('/::[a-zA-Z\d_\-]+\??::/','',$key);
+
         $admin=str_starts_with($key, 'admin.');
         return  str_replace('admin.', '', $key);
     }
     public static function input($key, \Closure|array|string $value)
     {
-        $key = self::getKey($key, self::$prefixes['inputs'], $admin);
+        $key = self::getKey($key, self::$prefixes['inputs'], $admin, $params);
         self::$inputs[$key]['value'] = $value;
         self::$inputs[$key]['admin'] = $admin;
+        self::$inputs[$key]['params'] = $params;
     }
     public static function callback($key, \Closure|array|string $value)
     {
-        $key = self::getKey($key, self::$prefixes['callbacks'], $admin);
+        $key = self::getKey($key, self::$prefixes['callbacks'], $admin, $params);
         self::$callbacks[$key]['value'] = $value;
         self::$callbacks[$key]['admin'] = $admin;
+        self::$callbacks[$key]['params'] = $params;
     }
     public static function any($key, \Closure|array|string $value)
     {
         self::input($key, $value);
         self::callback($key, $value);
+        self::step($key, $value);
     }
     public static function step($key, \Closure|array|string $value)
     {
-        $key = self::getKey($key, '', $admin);
+        $key = self::getKey($key, '', $admin, $params);
         self::$steps[$key]['value'] = $value;
         self::$steps[$key]['admin'] = $admin;
+        self::$steps[$key]['params'] = $params;
     }
 
     private static function fallbackRoutes(){
