4420092463042ccb4f1b549040be03a5214750f3
Ahmed Salheia <ahmedsalheia.as@gmail.com>
2022-08-23 20:11:43 +0300

    updates

--- index.php
+++ index.php
@@ -11,8 +11,7 @@ if (@$_SERVER['CONTENT_TYPE']===null) {
     exit();
 }
 
-Router::input('/start::id::',function ($id) use ($bot){
-    return Response::sendMessage()->text($id??'id is null');
+Router::input('/start::id?::',function ($id) use ($bot){
     // inside the input use {{$bot->update()->message()}} to get your message data...
     $text = "Hi ".$bot->update()->message()->from()->username." Your ID is: ".$bot->update()->message()->from()->id;
     return Response::sendMessage()->text($text);
@@ -21,7 +20,7 @@ Router::input('/start::id::',function ($id) use ($bot){
 Router::callback('##hi', function () use ($bot){
     // inside the input use {{$bot->update()->callback()}} to get your callback data, and {{$bot->update()->callback()->message()}} to get message data...
     $text = "Hi ".$bot->update()->callback()->message()->from()->username." Your ID is: ".$bot->update()->callback()->message()->from()->id;
-    return Response::sendMessage()->text('$text');
+    return Response::sendMessage()->text($text);
 });
 
 Router::initialize();
