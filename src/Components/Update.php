f928e22515178ddb85015826ed149f9c344c6b66
Ahmed Salheia <ahmedsalheia.as@gmail.com>
2022-08-25 20:18:42 +0300

    made a route function

--- src/Components/Update.php
+++ src/Components/Update.php
@@ -23,7 +23,7 @@ class Update
 
     public function getRoute()
     {
-        $data = explode(' ',$this->message()->text()??$this->callback()->data(), 2);
+        $data = explode(' ',$this->message()?->text()??$this->callback()?->data(), 2);
         return ($this->message!==null)?
             ['type'=>'input','route'=>array_shift($data), 'args'=>$data]:
             ['type'=>'callback','route'=>array_shift($data), 'args'=>$data];
